bcdcce4b020d7789e754fea47cab90c89369b5c7
onlinevikas19 <84133074+onlinevikas19@users.noreply.github.com>
2021-10-02 05:53:46 -0700

    day 2 complete

---
+++ lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = 'TestApplicaiton';
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('welcome to $days of $name')),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_applicaiton/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Demo Applicaiton'),
+      home: HomePage(),
     );
   }
 }
