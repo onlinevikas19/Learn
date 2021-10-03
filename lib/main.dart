9a69101bad11744ee93627e1acec4b62b2fe1e31
onlinevikas19 <84133074+onlinevikas19@users.noreply.github.com>
2021-10-03 00:39:10 -0700

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:first_applicaiton/home_page.dart';
+import 'package:first_applicaiton/pages/home_page.dart';
+import 'package:first_applicaiton/pages/login_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +8,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      /*themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),*/
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final int days = 30;
     final String name = 'TestApplicaiton';
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Demo Applicaiton'),
+      ),
       body: Center(child: Text('welcome to $days of $name')),
       drawer: Drawer(),
     );
